406273c9c4f982edd189c7c23d535dfcc2062b56
Railson Martins <railsonmartins1970@gmail.com>
2025-06-25 11:15:50 -0300

    Improves code formatting

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,16 +4,13 @@ describe("Login", () => {
   });
 
   it("Login com nome e senha válidos e verifica redirecionamento para area de segurança", () => {
-    cy.get("#username").type("tomsmith")
-    cy.get("#password").type("SuperSecretPassword!")
-    cy.contains('button', 'Login')
-      .should('be.visible')
-      .click()
+    cy.get("#username").type("tomsmith");
+    cy.get("#password").type("SuperSecretPassword!");
+    cy.contains("button", "Login").should("be.visible").click();
 
-    cy.contains(
-      '#flash', 
-      "You logged into a secure area!"
-      ).should('be.visible')
+    cy.contains("#flash", "You logged into a secure area!").should(
+      "be.visible"
+    );
   });
 
   it("Login com nome válido e senha inválida", () => {
