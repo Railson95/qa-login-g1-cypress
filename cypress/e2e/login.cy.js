bd9648344f265ffa89f2cbe9e3f205a0b2efd132
Railson Martins <railsonmartins1970@gmail.com>
2025-06-25 10:59:32 -0300

    Add test case for valid name and password

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,1 +1,18 @@
+describe("Login", () => {
+  beforeEach(() => {
+    cy.visit("https://the-internet.herokuapp.com/login");
+  });
 
+  it("Login com nome e senha válidos e verifica redirecionamento para area de segurança", () => {
+    cy.get("#username").type("tomsmith")
+    cy.get("#password").type("SuperSecretPassword!")
+    cy.contains('button', 'Login')
+      .should('be.visible')
+      .click()
+
+    cy.contains(
+      '#flash', 
+      "You logged into a secure area!"
+      ).should('be.visible')
+  });
+});
